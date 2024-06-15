97758702d56a2ac025cc482e0a9b65baf503f7ed
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2024-06-16 02:47:13 +1000

    Cleanup audio file

--- src/audio.ts
+++ src/audio.ts
@@ -1,21 +1,5 @@
-import { easeInOutQuad } from './animation';
 import { getCurrentPropValue } from './property';
 import { audio, scene } from './types';
-
-// audio: [
-//   {
-//     src: 'assets/sfx/rocky.wav',
-//     isLooped: true,
-//     start: 36%,
-//     stop: 42%,
-//     props: [{
-//         type: 'gain',
-//         values: [0, 0, 100, 0, 0],
-//         keys: ['0%', '40%', '50%', '60%', '100%'],
-//         isMotionControlled: true
-//     }],
-//   }
-// ],
 
 const getAudioFile = async (audioContext, filepath) => {
   const response = await fetch(filepath);
@@ -24,33 +8,7 @@ const getAudioFile = async (audioContext, filepath) => {
   return audioBuffer;
 };
 
-// https://developer.mozilla.org/en-US/docs/Web/API/AudioBufferSourceNode
-//
-const playSample = (audioContext, audioBuffer, time) => {
-  const sampleSource = new AudioBufferSourceNode(audioContext, {
-    buffer: audioBuffer,
-    loop: false,
-    playbackRate: 1, // Speed setting
-  });
-
-  const gainNode = audioContext.createGain();
-  sampleSource.connect(gainNode);
-  gainNode.connect(audioContext.destination);
-  gainNode.gain.setValueAtTime(0.2, audioContext.currentTime);
-
-  const gainNode2 = audioContext.createGain();
-  gainNode.connect(gainNode2);
-  gainNode2.connect(audioContext.destination);
-  gainNode2.gain.setValueAtTime(1, audioContext.currentTime);
-
-  // sampleSource.connect(audioContext.destination);
-
-  sampleSource.start();
-  return sampleSource;
-};
-
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Advanced_techniques#dial_up_%E2%80%94_loading_a_sound_sample
-
 const initAudioSample = async (audioObj: audio) => {
   const audioCtx = new AudioContext();
   audioCtx.onstatechange = () => {
@@ -71,6 +29,7 @@ const initAudioSample = async (audioObj: audio) => {
   };
 };
 
+// https://developer.mozilla.org/en-US/docs/Web/API/AudioBufferSourceNode
 const initAudioSource = (audioContext, audioBuffer, options) => {
   const sampleSource = new AudioBufferSourceNode(audioContext, {
     buffer: audioBuffer,
@@ -255,17 +214,6 @@ export const manageSceneAudio = (
     // TODO Handle a check here to see if we should start loading the audio on
     // page load or lazy load it later???
   });
-
-  // const gainNode = audioCtx.createGain();
-
-  // // create Oscillator node
-  // const oscillator = audioCtx.createOscillator();
-
-  // oscillator.type = 'square';
-  // oscillator.frequency.setValueAtTime(3000, audioCtx.currentTime); // value in hertz
-  // oscillator.connect(audioCtx.destination);
-  // oscillator.start();
-  // console.log('derp');
 };
 
 export const initSceneAudio = (scene) => {
