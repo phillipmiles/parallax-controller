0472e0ecb60cbb9985ee2f239fad12bdb78dd031
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2024-06-16 02:11:07 +1000

    Init effect nodes succesffully

--- src/audio.ts
+++ src/audio.ts
@@ -38,13 +38,16 @@ const playSample = (audioContext, audioBuffer, time) => {
   gainNode.connect(audioContext.destination);
   gainNode.gain.setValueAtTime(0.2, audioContext.currentTime);
 
+  const gainNode2 = audioContext.createGain();
+  gainNode.connect(gainNode2);
+  gainNode2.connect(audioContext.destination);
+  gainNode2.gain.setValueAtTime(1, audioContext.currentTime);
+
   // sampleSource.connect(audioContext.destination);
 
   sampleSource.start();
   return sampleSource;
 };
-
-// SOURCES ARRAY~!!!!!!
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Advanced_techniques#dial_up_%E2%80%94_loading_a_sound_sample
 
@@ -66,6 +69,15 @@ const initAudioSample = async (audioObj: audio) => {
     buffer: audioBuffer,
     sources: [],
   };
+};
+
+const initAudioSource = (audioContext, audioBuffer, options) => {
+  const sampleSource = new AudioBufferSourceNode(audioContext, {
+    buffer: audioBuffer,
+    loop: options.loop ? options.loop : false,
+    playbackRate: 1, // Speed setting
+  });
+  return sampleSource;
 };
 
 const applyAudioProp = (audioObj, prop, relativeScrollTop, value) => {
@@ -92,7 +104,7 @@ const applyAudioProps = (audioObj, relativeScrollTop) => {
   });
 };
 
-export const canTriggerAudio = (
+export const triggeredByDirection = (
   currentPos,
   prevPos,
   triggerPos,
@@ -103,17 +115,84 @@ export const canTriggerAudio = (
     currentPos >= triggerPos &&
     triggerDirection !== 'backwards'
   ) {
-    console.log('TRIGGER FORWARDS');
     return true;
   } else if (
     prevPos > triggerPos &&
     currentPos <= triggerPos &&
     triggerDirection !== 'forwards'
   ) {
-    console.log('TRIGGER BACKWARDS');
     return true;
   }
   return false;
+};
+
+export const initGrainNode = (audioObj, source, value) => {
+  const gainNode = audioObj.sample.context.createGain();
+  source.effectNodes[source.effectNodes.length - 1].connect(gainNode);
+
+  gainNode.gain.setValueAtTime(value, audioObj.sample.context.currentTime);
+
+  source.effectNodes.push(gainNode);
+};
+
+export const initAudioProps = (audioObj, source, relativeProgress) => {
+  audioObj.props.forEach((prop) => {
+    const value = getCurrentPropValue(prop, relativeProgress);
+    console.log('VALUE!!!', value);
+    if (prop.type === 'gain') {
+      initGrainNode(audioObj, source, value);
+    }
+
+    // TODO::: Maybe instead collect an array of the calculated values to
+    // be applied to the audio object or anmation selector and apply them
+    // all at once. That way we can apply all css transforms in one line and
+    // and maybe multiple audio effects at once on the same sample.
+    // applyAudioProp(audioObj, prop, relativeProgress, value);
+  });
+};
+
+export const triggerAudioSource = (audioObj, relativeProgress) => {
+  const sourceNode = initAudioSource(
+    audioObj.sample.context,
+    audioObj.sample.buffer,
+    {
+      loop: false,
+    }
+  );
+
+  const source = {
+    sourceNode: sourceNode,
+    effectNodes: [],
+  };
+
+  // Create volume node
+  // if (audioObj.volume) {
+  const volumeNode = audioObj.sample.context.createGain();
+  sourceNode.connect(volumeNode);
+
+  volumeNode.gain.setValueAtTime(
+    audioObj.volume >= 0 ? audioObj.volume : 1,
+    audioObj.sample.context.currentTime
+  );
+  source.effectNodes.push(volumeNode);
+  // }
+
+  initAudioProps(audioObj, source, relativeProgress);
+
+  source.effectNodes[source.effectNodes.length - 1].connect(
+    audioObj.sample.context.destination
+  );
+  sourceNode.start();
+
+  // Remove reference to source once its finished playing
+  sourceNode.addEventListener('ended', (event) => {
+    const sourceIndex = audioObj.sample.sources.findIndex(
+      (item) => item.sourceNode === event.target
+    );
+    audioObj.sample.sources.splice(sourceIndex, 1);
+  });
+  // Store source so we can keep track of how many we're playing.
+  audioObj.sample.sources.unshift(source);
 };
 
 export const manageSceneAudio = (
@@ -122,70 +201,53 @@ export const manageSceneAudio = (
   prevScrollTop
 ) => {
   if (!scene.audio) return;
+
+  // checks if user has ever interacted
+  // https://developer.mozilla.org/en-US/docs/Web/API/UserActivation
+  if (!navigator.userActivation.hasBeenActive) return;
+
   // console.log('relativeScrollTop', relativeScrollTop, window.scrollY);
   scene.audio.forEach((audioObj) => {
     // Adjust scroll top to be relative to any start/stop values if availalble.
-    const relativeScrollTop =
+    const relativeProgress =
       audioObj.start && typeof audioObj.start === 'number'
         ? sceneScrollTop - audioObj.start
         : sceneScrollTop;
 
-    const prevRelativeScrollTop =
+    const prevRelativeProgress =
       audioObj.start && typeof audioObj.start === 'number'
         ? prevScrollTop[0] - audioObj.start
         : prevScrollTop[0];
 
     if (audioObj.props) {
-      applyAudioProps(audioObj, relativeScrollTop);
+      applyAudioProps(audioObj, relativeProgress);
     }
 
+    // TODO: If range is supplied and a boolean is true make sounds stop
+    // that have left range.
+
     if (
-      canTriggerAudio(
-        relativeScrollTop,
-        prevRelativeScrollTop,
+      triggeredByDirection(
+        relativeProgress,
+        prevRelativeProgress,
         audioObj.trigger,
         audioObj.triggerDirection
       )
     ) {
-      console.log('TRIGGER', audioObj);
-      if (
-        audioObj.sample &&
-        // audioObj.sample.state === 'ready' &&
-        navigator.userActivation.hasBeenActive // checks if user has ever interacted
-        // https://developer.mozilla.org/en-US/docs/Web/API/UserActivation
-      ) {
-        // console.log('play', audioObj.sample.sources.length);
-
+      if (audioObj.sample) {
         if (
           !audioObj.maxPlaying ||
           audioObj.maxPlaying > audioObj.sample.sources.length
         ) {
-          const source = playSample(
-            audioObj.sample.context,
-            audioObj.sample.buffer,
-            0
-          );
-
-          source.addEventListener('ended', (event) => {
-            const sourceIndex = audioObj.sample.sources.findIndex(
-              (source) => source === event.target
-            );
-            audioObj.sample.sources.splice(sourceIndex, 1);
-          });
-          // Store source so we can keep track of how many we're playing.
-          audioObj.sample.sources.unshift(source);
+          triggerAudioSource(audioObj, relativeProgress);
         }
+      } else {
+        console.warn('Attempted to trigger audio source but no sample found.');
       }
     }
 
-    // console.log(sceneScrollTop, relativeScrollTop, audioObj.trigger);
-
     // TODO Handle a check here to see if we should start loading the audio on
     // page load or lazy load it later???
-    // if (!audioObj.sample) {
-    //   console.log('INIT AUDIO');
-    //   initAudioSample(audioObj);
-    // }
   });
 
   // const gainNode = audioCtx.createGain();
